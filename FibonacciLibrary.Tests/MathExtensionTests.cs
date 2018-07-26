28c48380698b644acef3427890d50c6ab2bcfd87
Ivan <ivan.trashchenko@gmail.com>
2018-07-27 02:05:14 +0300

    Add Task 1

---
+++ FibonacciLibrary.Tests/MathExtensionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+using NUnit.Framework;
+
+namespace FibonacciLibrary.Tests
+{
+    [TestFixture]
+    public class MathExtensionTests
+    {
+        [Test]
+        public void FibonacciGenerator_Success()
+        {
+            BigInteger[][] expected =
+                {
+                    new BigInteger[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 },
+                    new BigInteger[] { 0 },
+                    new BigInteger[] { 0, 1 }
+                };
+
+            for (int i = 0; i < 3; i++)
+            {
+                CollectionAssert.AreEqual(expected[i], MathExtension.FibonacciGenerator(expected[i].Length));
+            }
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void FibonacciGenerator_ThrowsArgumentOutOfRangeException(int amount)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => MathExtension.FibonacciGenerator(amount));
+        }
+    }
+}
---
+++ FibonacciLibrary/MathExtension.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace FibonacciLibrary
+{
+    /// <summary>
+    /// Class of FibonacciGenerator method.
+    /// </summary>
+    public static class MathExtension
+    {
+        #region FibonacciGenerator method
+
+        /// <summary>
+        /// Method for generating a sequence of Fibonacci numbers.
+        /// </summary>
+        /// <param name="amount">Amount of Fibonacci numbers.</param>
+        /// <returns>Sequence of type IEnumerable - BigInteger.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when amount value is less or equal to zero.</exception>
+        public static IEnumerable<BigInteger> FibonacciGenerator(int amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(amount)} must be positive.");
+            }
+
+            return GenerateSequence(amount);
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static IEnumerable<BigInteger> GenerateSequence(int amount)
+        {
+            BigInteger current = 0;
+            BigInteger next = 1;
+
+            for (int i = 0; i < amount; i++)
+            {
+                yield return current;
+                BigInteger temp = next;
+                next += current;
+                current = temp;
+            }
+        }
+
+        #endregion
+    }
+}
