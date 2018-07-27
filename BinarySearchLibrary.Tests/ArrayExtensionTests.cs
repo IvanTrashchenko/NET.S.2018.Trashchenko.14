c38af9ab9be6736cf1ed305cd1420d8b339df9fe
Ivan <ivan.trashchenko@gmail.com>
2018-07-27 20:00:40 +0300

    Add Task 2

---
+++ BinarySearchLibrary.Tests/ArrayExtensionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BinarySearchLibrary.Tests
+{
+    [TestFixture]
+    public class ArrayExtensionTests
+    {
+        [TestCase(new int[] { 1, 6, 15, 78, 79, 100 }, 78, ExpectedResult = 3)]
+        [TestCase(new int[] { 1, 6, 15, 78, 79, 100 }, 1, ExpectedResult = 0)]
+        [TestCase(new int[] { }, 15, ExpectedResult = null)]
+        public static int? BinarySearch_Int_Interface_Success(int[] array, int item) 
+            => array.BinarySearch(item);
+
+
+        [TestCase(new string[] { "a", "b", "c", "d", "e" }, "d", ExpectedResult = 3)]
+        [TestCase(new string[] { "a", "b", "c", "d", "e" }, "", ExpectedResult = null)]
+        public static int? BinarySearch_String_Delegate_Success(string[] array, string item) 
+            => array.BinarySearch(item, string.Compare);
+
+        [TestCase(null, "d")]
+        public void BinarySearch_ThrowsArgumentNullException(string[] array, string item)
+        {
+            Assert.Throws<ArgumentNullException>(() => array.BinarySearch(item));
+        }
+    }
+}
---
+++ BinarySearchLibrary/ArrayExtension.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearchLibrary
+{
+    /// <summary>
+    /// Class for BinarySearch methods.
+    /// </summary>
+    public static class ArrayExtension
+    {
+        /// <summary>
+        /// Method which performs binary search of specified item in a sorted array.
+        /// </summary>
+        /// <typeparam name="T">Type of items.</typeparam>
+        /// <param name="sortedArray">Sorted array.</param>
+        /// <param name="item">Item to find.</param>
+        /// <param name="comparer">Comparer of items.</param>
+        /// <returns>Index of item of type <see cref="int"/> if it was found; otherwise, null.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when source array is null.</exception>
+        public static int? BinarySearch<T>(this T[] sortedArray, T item, IComparer<T> comparer = null)
+        {
+            if (sortedArray == null)
+            {
+                throw new ArgumentNullException($"{nameof(sortedArray)} can not be null.");
+            }
+
+            if (sortedArray.Length == 0)
+            {
+                return null;
+            }
+
+            if (comparer == null)
+            {
+                comparer = Comparer<T>.Default;
+            }
+
+            int first = 0;
+            int last = sortedArray.Length;
+
+            while (first < last)
+            {
+                int middle = first + ((last - first) / 2);
+
+                if (comparer.Compare(item, sortedArray[middle]) == 0)
+                {
+                    return middle;
+                }
+
+                if (comparer.Compare(item, sortedArray[middle]) < 0)
+                {
+                    last = middle;
+                }
+                else
+                {
+                    first = middle + 1;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Method which performs binary search of specified item in a sorted array.
+        /// </summary>
+        /// <typeparam name="T">Type of items.</typeparam>
+        /// <param name="sortedArray">Sorted array.</param>
+        /// <param name="item">Item to find.</param>
+        /// <param name="comparison">Comparison delegate.</param>
+        /// <returns>Index of item of type <see cref="int"/> if it was found; otherwise, null.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when source array is null.</exception>
+        public static int? BinarySearch<T>(this T[] sortedArray, T item, Comparison<T> comparison) =>
+            sortedArray.BinarySearch(item, Comparer<T>.Create(comparison));
+    }
+}
